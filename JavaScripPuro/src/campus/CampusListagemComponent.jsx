c658d6911be44d4ccd969f42dbcfc0fd9a25a2bb
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-06-16 22:35:56 -0400

    🚨 🧐 Atualizando 13/06/2023

--- JavaScripPuro/src/campus/CampusListagemComponent.jsx
+++ JavaScripPuro/src/campus/CampusListagemComponent.jsx
@@ -1,38 +1,36 @@
 import { Container, Stack, Typography } from "@mui/material";
 import React from "react";
-import api from "../api/api"
-export default function CampusListagemComponent(){
+import api from "../api/api";
+export default function CampusListagemComponent() {
+  const [dados, setDados] = React.useState({ id: -1, nome: "" });
+  const [contagem, setContagem] = React.useState(0);
 
-    const [ dados,setDados ] = React.useState({id: -1, nome: ''});
-
-   React.useEffect(async() => )
-   api.get("/campus/list")
-   .then(response => {
-      // manipula o sucesso da requisição
+  React.useEffect(async () => {
+    api.get("/campus/list")
+      .then((response) => {
+        // manipula o sucesso da requisição
         setDados(response.data);
-    })
-    .catch(error => console.log(error));
-console.log(dados);
+      })
+      .catch((error) => console.log(error));
+  })[contagem, setContagem];
 
-    return (
-        <Container sx= {{p:3, m: 3}}>
-            <Paper elevation={2}>
-                <Box sx={{
-                    p:3,
-                    m:3
-                 }} >
-                    <Typography variant="h3">
-                        Consulta de Campi
-                    </Typography>
-                </Box>
-                <Stack>
-                    {dados.map(item => (
-                        <Typography variant="subtitle1">
-                            {item.nome}
-                        </Typography>
-                    ))}
-                </Stack>
-            </Paper>
-        </Container>
-    )
+  return (
+    <Container sx={{ p: 3, m: 3 }}>
+      <Paper elevation={2}>
+        <Box
+          sx={{
+            p: 3,
+            m: 3,
+          }}
+        >
+          <Typography variant="h3">Consulta de Campi</Typography>
+        </Box>
+        <Stack>
+          {dados.map((item) => (
+            <Typography variant="subtitle1">{item.nome}</Typography>
+          ))}
+        </Stack>
+      </Paper>
+    </Container>
+  );
 }
