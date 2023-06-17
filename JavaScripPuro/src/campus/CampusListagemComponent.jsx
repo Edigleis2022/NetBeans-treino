c4f9c221e177b34c3be1ba44a0d75254fd878c62
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-06-16 22:11:29 -0400

    ⚡️ Codigo Corrigido

--- JavaScripPuro/src/campus/CampusListagemComponent.jsx
+++ JavaScripPuro/src/campus/CampusListagemComponent.jsx
@@ -1,7 +1,18 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
-
+import api from "../api/api"
 export default function CampusListagemComponent(){
+
+    const [ dados,setDados ] = React.useState({id: -1, nome: ''});
+
+   React.useEffect(async() => )
+   api.get("/campus/list")
+   .then(response => {
+      // manipula o sucesso da requisição
+        setDados(response.data);
+    })
+    .catch(error => console.log(error));
+console.log(dados);
 
     return (
         <Container sx= {{p:3}}>
